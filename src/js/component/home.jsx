55c473f5cb2d114e81c869dd20b2d23518166db5
Dorian Zuluaga <166706826+Dorianzuluaga@users.noreply.github.com>
2024-12-15 13:22:40 +0000

    todo-list-Dorian

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import React from "react";
 
 //include images into your bundle
@@ -5,22 +6,55 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const [state, setState] = useState([]);
+  const [input, setInput] = useState('');
+
+  const add = () => {
+	setState([...state, input]);
+    setInput('');
+  };
+
+  const deleteTask = (index) => {
+    const filtered = state.filter((element, i) => {
+      return i != index;
+    });
+    setState(filtered);
+  };
+
+  return (
+    <div className="todo-list">
+      <h1>TODO LIST</h1>
+      <div className="input-add">
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        type="text"
+        placeholder="Add Name"
+      ></input>
+      <button onClick={add}>+</button>
+      </div>
+      <ul>
+        {state.length <= 0
+          ? 'No Tasks'
+          : state.map((item, index) => {
+              return (
+                <div>
+                  <li>
+                    {item}{' '}
+                    <button className="delete"
+                      onClick={() => {
+                        deleteTask(index);
+                      }}
+                    >
+                      x
+                    </button>
+                  </li>
+                </div>
+              );
+            })}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
